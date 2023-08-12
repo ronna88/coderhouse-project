b4f20b61f907d95edf679ce4aa934bda4ce4d48e
Theo <ronna88@gmail.com>
2023-08-11 21:43:55 -0300

    alteração de css menu

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,28 @@
+
+import "./style.css";
+
+function NavBar() {
+    return (
+        <div className="container">
+            <div className="content">
+                <div className="brand-nav">Coder Clothes</div>
+                <div>
+                    <div className="menu-nav">
+                        <div className="menu-item-nav">
+                            <a className="menu-item-link-nav" href="#">Home</a>
+                        </div>
+                        <div className="menu-item-nav">
+                            <a className="menu-item-link-nav" href="#">Roupas Masculinas</a>
+                        </div>
+                        <div className="menu-item-nav">
+                            <a className="menu-item-link-nav" href="#">Roupas Femininas</a>
+                        </div>
+                    </div>
+                </div>
+                <div><button className="login-nav">Login</button></div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
